468e1e0a0ec3fadae60b42c2fd890adaa769f12a
Ashutoshdocs <aksinghdocument@gmail.com>
2026-07-03 08:35:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Azure DevOps Demo"
+    return render_template("index.html")
 
 @app.route("/health")
 def health():
